2900683f7986046b4fe6a204af557b1578824ef1
christina <christina.sherman@seattlecolleges.edu>
2025-01-25 21:53:14 -0800

    functioning db and pages

--- WeekTwo/expressHW/index.js
+++ WeekTwo/expressHW/index.js
@@ -1,29 +1,43 @@
 "use strict";
+import mongoose from "mongoose"; 
+import express from "express";
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import {Album} from './models/album.js';
 
-let albums = require('./data.js');
-const express = require("express");
+dotenv.config();
+
+
 const app = express();
-const port = 3000;
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "public"));
+
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
+app.use('/api',cors());
+app.set("view engine", "ejs");
 
 
-// home page
-app.get('/', (req, res) => {
-    res.render('pages/home', {albums: albums.getAllAlbums()});
-})
+app.get('/' || '/albums', (req, res) => {
+    Album.find({}).lean()
+    .then((albums) => {
+        res.render('home', {albums: albums});
+    })
+        .catch((err) => {
+            console.error(err);
+        })
+});
+
 
 //details page
-app.get('/detail', (req,res) => {
-    const result = albums.getAlbum(req.query.artist);
-    res.render("pages/details", {
-            artist: req.query.artist,
-            result,
-            albums: albums.getAllAlbums()
-        }
-    );
+app.get('/detail', (req, res, next) => {
+    Album.findOne({ artist:req.query.artist}).lean()
+    .then((album) => {
+        res.render('details', {album: album});
+    })
+    .catch((err) => {next(err)})
 });
 
 
@@ -34,8 +48,7 @@ app.use((req,res,next)=>{
 });
 
 
-module.exports = app;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+app.listen(app.get('port'), () => {
+    console.log(`Express started`);
 })
 
